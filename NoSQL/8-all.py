fadd6cf9238b9d7951aff8d80a1e0c1137530921
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 20:20:11 -0500

    holberton

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,8 +2,8 @@
 """intro of the module"""
 
 from pymongo import MongoClient
-def list_all(mongo_collection)
-"""using find to list all documents"""
 
-	listed=collection.find({})
-	return listed
+def list_all(mongo_collection):
+    """using find to list all documents"""
+    listed = mongo_collection.find({})
+    return listed
