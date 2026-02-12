7b6b0a859023fa8eed3d217bf36591177ecf0e34
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 21:27:03 -0500

    holberton

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 """
-this module provides stats about Nginx logs stored in MongoDB.
-it connects to the logs database and analyzes the nginx collection.
+statistical data provider
 """
+
 from pymongo import MongoClient
 
 
-def statistica():
-    """
-    we calculate the total number of logs and also print out other statistics
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+def analyze_nginx_logs():
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
+
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
@@ -27,4 +24,4 @@ def statistica():
 
 
 if __name__ == "__main__":
-    statistica()
+    analyze_nginx_logs()
