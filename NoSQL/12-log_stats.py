152e8270b8b7974f02fd2f2e86d0a0177ec5f288
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 21:18:19 -0500

    holberton

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,22 +4,30 @@
 from pymongo import MongoClient
 
 def get_nginx_stats():
-    """Fetches stats from the Nginx logs in MongoDB.
-       Counts the total number of logs and breaks it down by HTTP method.
-       Also counts the number of GET requests to the /status path."""
+    """Fetches stats from the Nginx logs in MongoDB."""
     client = MongoClient("mongodb://localhost:27017/")
     db = client.logs
     collection = db.nginx
-    """we count the total logs using count function and the same for every method"""
+    
     total_logs = collection.count_documents({})
+    
+    if total_logs == 0:
+        print("0 logs")
+        print("Methods:")
+        for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+            print(f"\tmethod {method}: 0")
+        print("GET /status: 0 logs")
+        return
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     method_counts = {method: collection.count_documents({"method": method}) for method in methods}
     get_status_count = collection.count_documents({"method": "GET", "path": "/status"})
-    """we print total logs and using loop print ip for every other method"""
+    
     print(f"{total_logs} logs")
+    print("Methods:")
     for method in methods:
-        print(f"\t{method}: {method_counts[method]} logs")
+        print(f"\tmethod {method}: {method_counts[method]}")
     print(f"GET /status: {get_status_count} logs")
+
 if __name__ == "__main__":
     get_nginx_stats()
-	
