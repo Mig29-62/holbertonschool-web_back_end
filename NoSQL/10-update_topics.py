47a25f18334b11b74f16f34c76a16c285d34ead4
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 20:49:04 -0500

    holberton

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """Update topics """
-    result = mongo_collection.update_one(
-        {'name': name, '$or': [{'topics': {'$exists': False}}, {'topics': []}]}, 
+    result = mongo_collection.update_many(
+        {'name': name}, 
         {'$set':{'topics':topics}}
     )
